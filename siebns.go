8317b88532d9cb8e096d1f70c4f044ddffaa55c6
Rustam Gilyazov <rustam.gilyazov@dbvisit.com>
2018-01-15 18:58:44 +1300

    siebns package

---
+++ siebns.go
@@ -0,0 +1,186 @@
+package siebns
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// NSFileError used to return errors
+type NSFileError string
+
+// NSFixer interface for fixing Files
+type NSFixer interface {
+	FixSize()
+	Load(path string) error
+	Close()
+}
+
+// NSFile Name Server File descriptor
+type NSFile struct {
+	Name             string   // filename and path
+	Size             int64    // real file size
+	fmtUnicode       bool     // true if file has BOM
+	fmtDos           bool     // true if file has dos line endings
+	fmtLittleEndian  bool     // true if file is little-endian (x86)
+	CorrectionNeeded bool     // true if checksum is invalid
+	offsetHeader     byte     // header offset
+	offsetChecksum   int64    // checksum value offset
+	offsetData       int      // actual data offset
+	f                *os.File // file handle
+}
+
+func (e NSFileError) Error() string {
+	return fmt.Sprintf("Error: %s", string(e))
+}
+
+//FixSize fixes the size in header
+func (ns *NSFile) FixSize() (int, error) {
+	buffer := make([]byte, 12)
+	ns.f.ReadAt(buffer, ns.offsetChecksum)
+	encoded, err := ns.encodeSize()
+	if err != nil {
+		return 0, err
+	}
+	ns.f.Seek(ns.offsetChecksum, os.SEEK_SET)
+	wrote, err := ns.f.Write(encoded)
+	if err != nil {
+		return 0, err
+	}
+	ns.CorrectionNeeded = false
+	return wrote, nil
+}
+
+// Close the file
+func (ns *NSFile) Close() {
+	ns.f.Close()
+}
+
+// Load loads an ns file and verifies that it's valid
+func (ns *NSFile) Load(path string) error {
+	var err error
+	fileInfo, serr := os.Stat(path)
+	if serr != nil {
+		return (serr)
+	}
+	if fileInfo.Size() <= 0x3e {
+		return NSFileError("Not a Siebel Gateway file.")
+	}
+	ns.Size = fileInfo.Size()
+
+	ns.f, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	ns.Name = path
+
+	err = ns.parseHeader()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// decodeSize determines the endianness and returns the valid file size
+func decodeSize(b []byte) (int64, bool, error) {
+	var size int64
+	base64Size, err := base64.StdEncoding.DecodeString(
+		strings.Trim(string(b[0:]), " \r\n"))
+	if err != nil {
+		return 0, false, err
+	}
+	buf := bytes.NewReader(base64Size)
+	err = binary.Read(buf, binary.LittleEndian, &size)
+	if err != nil {
+		return 0, false, err
+	}
+	littleEndian := !(size > int64(4294967296))
+	if !littleEndian {
+		buf.Seek(0, os.SEEK_SET)
+		err = binary.Read(buf, binary.BigEndian, &size)
+		if err != nil {
+			return 0, false, err
+		}
+	}
+	return size, littleEndian, err
+}
+
+func (ns *NSFile) parseHeader() error {
+	ns.offsetHeader = 0
+	ns.fmtDos = false
+	ns.fmtUnicode = false
+	ns.CorrectionNeeded = false
+	currentPos := 0
+
+	// read format line
+	ns.f.Seek(0, os.SEEK_SET)
+	reader := bufio.NewReader(ns.f)
+	line, err := reader.ReadBytes('\n')
+	if err != nil {
+		return err
+	}
+	currentPos += len(line)
+
+	// unicode detection
+	if line[0] == 0xef && line[1] == 0xbb && line[2] == 0xbf {
+		ns.fmtUnicode = true
+		ns.offsetHeader = 3
+	}
+	// signature
+	if string(line[ns.offsetHeader:6+ns.offsetHeader]) != "Siebel" {
+		return NSFileError("Not a Siebel Gateway file.")
+	}
+	// line endings
+	if line[len(line)-2] == '\r' {
+		ns.fmtDos = true
+	}
+
+	// skip 2 lines and read the base64 value
+	for i := 0; i < 2; i++ {
+		line, err = reader.ReadBytes('\n')
+		if err != nil {
+			return err
+		}
+		currentPos += len(line)
+	}
+
+	ns.offsetChecksum = int64(currentPos)
+	line, err = reader.ReadBytes('\n')
+	if (ns.fmtDos && len(line) != 27) || (!ns.fmtDos && len(line) != 26) {
+		return NSFileError("Checksum part is corrupt.  Please fix it manually by\nopening the file in the editor and deleting data from line 4 (leaving the\nline 4 empty).")
+	}
+
+	var base64Size int64
+	base64Size, ns.fmtLittleEndian, err = decodeSize(line)
+	if base64Size != ns.Size || err != nil {
+		ns.CorrectionNeeded = true
+	}
+
+	return nil
+}
+
+func (ns *NSFile) encodeSize() ([]byte, error) {
+	var err error
+	if ns.Size == 0 {
+		panic("Zero file size")
+	}
+	buf := new(bytes.Buffer)
+
+	if ns.fmtLittleEndian {
+		err = binary.Write(buf, binary.LittleEndian, ns.Size)
+	} else {
+		err = binary.Write(buf, binary.BigEndian, ns.Size)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	output := base64.StdEncoding.EncodeToString(buf.Bytes())
+
+	return []byte(output), nil
+}
---
+++ siebns_test.go
@@ -0,0 +1,166 @@
+package siebns
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+type testVals struct {
+	size           int64
+	base64data     string
+	isLittleEndian bool
+}
+
+var testSet = []testVals{
+	{size: 780, base64data: "DAMAAAAAAAA=", isLittleEndian: true},
+	{size: 780, base64data: "AAAAAAAAAww=", isLittleEndian: false},
+}
+
+const (
+	testfileEmpty = `Siebel Name Server Backing File
+16.0.0.0 [23057] ENU
+1.2
+DAMAAAAAAAA=             
+
+[/]
+	Persistence=partial
+	Type=empty
+`
+	testfileCorrupt = "Siebel Name Server Backing File\n16.0.0.0 [23057] ENU\n1.2\nDA=     \n\n[/]\n	Persistence=partial\n	Type=empty"
+)
+
+func CreateTestNSFile(contents string) *NSFile {
+	f, err := ioutil.TempFile("", "siebns")
+	if err != nil {
+		panic("Unable to create test file")
+	}
+	f.Write([]byte(contents))
+	ns := NSFile{
+		Name: f.Name(),
+		Size: int64(len(contents)),
+		f:    f}
+	return &ns
+}
+
+func CloseTestNSFile(ns *NSFile) {
+	name := ns.f.Name()
+	ns.f.Close()
+	if err := os.Remove(name); err != nil {
+		panic(err)
+	}
+}
+
+func TestDecode(t *testing.T) {
+
+	for i, v := range testSet {
+		size, isLittleEndian, err := decodeSize([]byte(v.base64data))
+		if err != nil {
+			t.Errorf("Item %d: Got error: %v", i, err)
+		}
+		if size != v.size {
+			t.Errorf("Item %d: Size invalid: got: %d, want: %d.",
+				i, size, v.size)
+		}
+		if isLittleEndian != v.isLittleEndian {
+			t.Errorf("Item %d: isLittleEndian invalid: got: %v, want: %v.",
+				i, isLittleEndian, v.isLittleEndian)
+		}
+	}
+
+}
+
+func TestEncode(t *testing.T) {
+
+	ns := NSFile{}
+	for i, v := range testSet {
+		ns.Size = v.size
+		ns.fmtLittleEndian = v.isLittleEndian
+		encoded, err := ns.encodeSize()
+		if err != nil {
+			t.Errorf("Item %d: Got error: %v", i, err)
+		}
+		if string(encoded) != v.base64data {
+			t.Errorf("Item %d:  Invalid data:  got: %s, want: %s.", i,
+				encoded, v.base64data)
+		}
+	}
+}
+
+func TestNSFileError(t *testing.T) {
+	if testErr := NSFileError("Big mistake"); strings.Index("Big mistake", testErr.Error()) > 0 {
+		t.Fail()
+	}
+}
+
+func TestParseHeader(t *testing.T) {
+	ns := CreateTestNSFile(testfileEmpty)
+	defer CloseTestNSFile(ns)
+
+	if err := ns.parseHeader(); err != nil {
+		t.Fatal(err)
+	}
+	if !ns.CorrectionNeeded {
+		t.Error("CorrectionNeeded invalid")
+	}
+	if ns.fmtDos {
+		t.Error("fmtDos invalid")
+	}
+	if ns.fmtUnicode {
+		t.Error("fmtUnicode invalid")
+	}
+	if ns.fmtUnicode {
+		t.Error("fmtUnicode invalid")
+	}
+	if ns.offsetHeader != 0 {
+		t.Errorf("offsetHeader invalid: want: %d, got: %d.", 0, ns.offsetHeader)
+	}
+	if ns.offsetChecksum != 57 {
+		t.Errorf("offsetChecksum invalid: want: %d, got: %d.", 57, ns.offsetChecksum)
+	}
+}
+
+func TestParseHeaderCorrupt(t *testing.T) {
+	ns := CreateTestNSFile(testfileCorrupt)
+	defer CloseTestNSFile(ns)
+
+	if err := ns.parseHeader(); err != nil {
+		if strings.Index("Checksum part is corrupt", err.Error()) == 0 {
+			t.Error("Did not detect checksum line corruption.")
+		}
+	}
+}
+
+func TestFixSize(t *testing.T) {
+	ns := CreateTestNSFile(testfileEmpty)
+	defer CloseTestNSFile(ns)
+
+	if err := ns.parseHeader(); err != nil {
+		t.Fatal(err)
+	}
+	if !ns.CorrectionNeeded {
+		t.Error("CorrectionNeeded invalid")
+	}
+
+	bytes, err := ns.FixSize()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes != 12 {
+		t.Errorf("Wrong bytes count.  Want: %d, Got: %d.", 12, bytes)
+	}
+
+}
+
+func TestLoadClose(t *testing.T) {
+	ns := CreateTestNSFile(testfileEmpty)
+	if err := ns.Load(ns.Name); err != nil {
+		t.Fatal(err)
+	}
+	defer ns.Close()
+	if ns.Size != int64(len(testfileEmpty)) {
+		t.Errorf("Wrong Size.  Want: %d, Got: %d.", len(testfileEmpty), ns.Size)
+	}
+
+}
